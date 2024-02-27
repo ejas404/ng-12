971c9232c18c96e3ee126eb9b4fa18cd70047f0c
ejas404 <ijasjaaass004@gmail.com>
2024-02-27 18:10:16 +0530

    add actionlogs signal

--- day2/src/app/component/counter/counter.component.ts
+++ day2/src/app/component/counter/counter.component.ts
@@ -9,17 +9,17 @@ import {BtnComponent} from '../btn/btn.component'
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
-  actionLogs : string[] = []
+  actionLogs = signal<string[]>([]);
   counter  = signal(0)
 
   increment(){
     this.counter.update(val => val+1)
-    this.actionLogs.push("INCREMENT")
+    this.actionLogs.update(val => [...val,"INCREMENT"])
   }
 
   decrement(){
     if(this.counter() <= 0) return;
     this.counter.update(val => val-1)
-    this.actionLogs.push("DECREMENT")
+    this.actionLogs.update(val => [...val,"DECREMENT"])
   }
 }
